b209cd9b3fe10c3c32a3bcccb828c36e7f262472
imeab <qodbals18@gmail.com>
2026-08-24 17:51:09 +0900

    과제

--- models/specialized_books.py
+++ models/specialized_books.py
@@ -1,6 +1,9 @@
+from .base_book import Book
+
+
 class PaperBook(Book):
     def __init__ (self, title, author, isbn, pages):
-        super().__init__(self, title, author. isbn)
+        super().__init__(self, title, author, isbn)
         self.pages = pages
 
 
@@ -8,3 +11,13 @@ class PaperBook(Book):
         def display_info(self):
             base_info = super().display_info()
             return f"{base_info}, 페이지 수: {self.pages}p {단행본}"
+
+
+class EBook(Book):
+    def __init__ (self, title, author, isbn, file_size):
+        super().__init__(self, title, author, isbn)
+        self.file_size = file_size
+
+        def displat_info(self):
+            base_info = super().display_info()
+            return f"{base_info}, 파일 크기: {self.file_size}MB {전자도서}"
